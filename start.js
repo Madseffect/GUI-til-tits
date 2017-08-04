a9e9fc3e1aadc319ce50dae14366e6e389cf5846
Madseffect <themadsk@gmail.com>
2017-08-04 12:04:17 +0200

    fsdfs
    ss

--- start.js
+++ start.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
         firstDay: 1,
         defaultView: 'agendaWeek',
         minTime: '07:00:00',
-        maxTime:'21:00:00',
+        maxTime: '21:00:00',
         slotLabelFormat: 'H(:mm)',
         height: 740,
         events: [
-       
-        {
-             title  : 'event2',
-            start  : '2017-08-03T12:30:00'
-        },
-         {
-            title  : 'event1',
-            start  : '2017-08-09T12:30:00'          
 
-        }
-    ]
+            {
+                title: 'event2',
+                start: '2017-08-03T12:30:00'
+            },
+            {
+                title: 'event1',
+                start: '2017-08-09T12:30:00'
+
+            }
+        ]
 
 
     })
@@ -30,7 +30,9 @@ $(document).ready(function () {
         $('#modal-title').text($(this).text());
     })
 
-   
+    $("#target").keyup(function () {
+        
+    });
 
 
 });
